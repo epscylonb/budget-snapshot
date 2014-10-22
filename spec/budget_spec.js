a7af3c8ba71f3dab11c79d504ddc7e71cfee011d
eps <eps@epscylonb.com>
2014-10-22 12:05:19 +0800

    WIP

---
+++ spec/budget_spec.js
@@ -0,0 +1,73 @@
+JS.Test.describe('Budget', function() { with(this) {
+  before(function() { with(this) {
+    this.Budget = new Budget()
+  }})
+
+  describe('lines', function() { with(this) { 
+    // lines should be an empty array
+    it('returns 0', function() { with(this) {
+      assert( Budget.lines.length == 0 )
+    }}) 
+
+    // should be able to add to lines
+    it('returns 1', function() { with(this) {
+      var line = new Line(10, 1, 'Income')
+      Budget.addLine(line)
+      assert( Budget.lines.length == 1)
+    }})
+
+    // should not be able to add two lines with the same description
+    it('throws an error', function() { with(this) {      
+      Budget.lines = [] // empty lines
+      var line = new Line(10, 1, 'Income')
+      Budget.addLine(line)
+      assertThrows(Error, function() { 
+        var line = new Line(10, 1, 'Income')
+        Budget.addLine(line)
+      })
+    }})
+
+    it('returns 10', function() { with(this) {
+      Budget.lines = [] // empty lines
+      var line = new Line(10, 1, 'Income')
+      Budget.addLine(line)
+      assert( Budget.totalPerDay() == 10 )     
+    }}) 
+
+    it('returns 20', function() { with(this) {
+      Budget.lines = [] // empty lines
+      var line = new Line(20, 1, 'Income')
+      Budget.addLine(line)
+      assert( Budget.totalPerDay() == 20 )     
+    }}) 
+
+    it('returns 70', function() { with(this) {
+      Budget.lines = [] // empty lines
+      var line = new Line(10, 1, 'Income')
+      Budget.addLine(line)
+      assert( Budget.totalPerWeek() == 70 )     
+    }}) 
+
+    it('returns 300', function() { with(this) {
+      Budget.lines = [] // empty lines
+      var line = new Line(10, 1, 'Income')
+      Budget.addLine(line)
+      assert( Budget.totalPerMonth() == 300 )     
+    }}) 
+
+    it('returns 3650', function() { with(this) {
+      Budget.lines = [] // empty lines
+      var line = new Line(10, 1, 'Income')
+      Budget.addLine(line)
+      assert( Budget.totalPerYear() == 3650 )     
+    }}) 
+
+    it('returns -140', function() { with(this) {
+      Budget.lines = [] // empty lines
+      var line = new Line(-10, 1, 'Income1')
+      Budget.addLine(line)
+      var line = new Line(-10, 1, 'Income2')
+      Budget.addLine(line)
+      assert( Budget.totalPerWeek() == -140 )     
+  }})
+}}) 
---
+++ spec/line_spec.js
@@ -0,0 +1,33 @@
+JS.Test.describe('Line', function() { with(this) {
+
+  before(function() { with(this) {
+    this.line = new Line(10, 1, 'Income')
+  }})
+
+  describe('amount per year', function() { with(this) {
+    it('returns 3650', function() { with(this) {
+      assert( line.perYear() == 3650 ) 
+    }})
+
+    it('returns 70', function() { with(this) {
+      this.line = new Line(10, 1, 'Income')
+      assert( line.perWeek() == 70)
+    }})
+
+    it('returns 300', function() { with(this) {
+      this.line = new Line(10, 1, 'Income')
+      assert( line.perMonth() == 300)
+    }})
+
+    it('returns 10', function() { with(this) {
+      assert( line.perDay() == 10)
+    }})
+
+    it('returns -70', function() { with(this) {
+      this.line = new Line(-10, 1, 'Expense')
+      assert( line.perWeek() == -70)
+    }})
+
+  }})
+}})
+
